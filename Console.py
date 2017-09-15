ddf161dfe4d0f90bc0adb04ef1c20ace8d31165b
rfdenk <rfdenk@gmail.com>
2017-09-15 11:09:28 -0700

    Cleanup to conform with PEP8.
    Signed-off-by: rfdenk <rfdenk@gmail.com>

--- Console.py
+++ Console.py
@@ -5,8 +5,9 @@ import sys
 # version 3.2 doesn't support the 'flush' parameter,
 # but I like to use it!
 
+
 def pr(s, end='\n', flush=False):
     sys.stdout.write(s)
     sys.stdout.write(end)
-    if flush: sys.stdout.flush()
-
+    if flush:
+        sys.stdout.flush()
