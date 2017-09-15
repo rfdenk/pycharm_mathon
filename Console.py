5648e26ee8795e8d1fd587aeb0e05b9e0504bcf5
rfdenk <rfdenk@gmail.com>
2017-09-15 08:55:42 -0700

    Added comment
    Signed-off-by: rfdenk <rfdenk@gmail.com>

--- Console.py
+++ Console.py
@@ -1,6 +1,10 @@
 
 import sys
 
+# shim for 3.6.2 print() function
+# version 3.2 doesn't support the 'flush' parameter,
+# but I like to use it!
+
 def pr(s, end='\n', flush=False):
     sys.stdout.write(s)
     sys.stdout.write(end)
