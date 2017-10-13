d58cd6e477f9a78fc3a12165d193e4db9209d447
rfdenk <rfdenk@gmail.com>
2017-10-13 16:49:12 -0700

    GUI for this operation, using tkinter.

--- Node.py
+++ Node.py
@@ -7,7 +7,8 @@ __all__ = [
     'ModuloOperatorNode',
     'ExponentiationOperatorNode',
     'RightOperatorNode',
-    'ParenthesisNode'
+    'ParenthesisNode',
+    'processCommand'
 ]
 
 __author__ = 'RobertD'
@@ -475,3 +476,62 @@ class ParenthesisNode(OperatorNode):
         if self.endParen:
             d = d + ')'
         return d
+
+def processCommand(origRoot, origParenDepth, command):
+    # clone off the original info.
+    # if there is an error, we'll return the original.
+    root = origRoot.clone()
+    parenDepth = origParenDepth
+
+    first = True
+    try:
+        for k in command:
+            if k in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'):
+                if first and isinstance(root, NumberNode):
+                    # restart the formula
+                    root = NumberNode(int(k))
+                else:
+                    # continue the formula
+                    root = root.append(NumberNode(int(k)))
+            elif k == '.':
+                root = root.appendDecimalPoint()
+            elif k == '+':
+                root = root.append(AdditionOperatorNode(parenDepth))
+            elif k == '-':
+                root = root.append(SubtractionOperatorNode(parenDepth))
+            elif k == 'x':
+                root = root.append(MultiplicationOperatorNode(parenDepth))  # preferred symbol for multiply
+            elif k == '*':
+                root = root.append(MultiplicationOperatorNode(parenDepth))  # allowed alternative symbol for multiply
+            elif k == '/':
+                root = root.append(DivisionOperatorNode(parenDepth))
+            elif k == '%':
+                root = root.append(ModuloOperatorNode(parenDepth))
+            elif k == '^':
+                root = root.append(ExponentiationOperatorNode(parenDepth))
+            elif k == '(':
+                parenDepth += 1
+                if first and isinstance(root, NumberNode):
+                    # restart the formula
+                    # We need an operator to hold the parenthesized atom as a right child.
+                    # we could just use the AdditionOperatorNode, but then the printout would look
+                    # wrong: "4+3" ==> "0+4+3", or "(2x3)" ==> "0+(2x3)"
+                    # We use the "Right" pseudo-operator, that only prints and evaluates its right side.
+                    parenDepth = 1
+                    root = NumberNode(0)
+                    root = root.append(RightOperatorNode(0))
+                root = root.append(ParenthesisNode(parenDepth))
+            elif k == ')':
+                if parenDepth > 0:
+                    root = root.closeParen(parenDepth)
+                    parenDepth -= 1
+                else:
+                    print(") error")
+                    raise InvalidOperatorSequenceError()
+
+            first = False
+    except:
+        print("Error; try again")
+        return origRoot, origParenDepth
+
+    return root, parenDepth
